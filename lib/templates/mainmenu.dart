7433bb9a689791a33fa2b801d49fae38f91a23ef
jhon loza <jmloza.fis@unach.edu.ec>
2020-08-15 00:08:36 -0500

    menuInicial, ingresar funciones botones seleccion

--- lib/templates/mainmenu.dart
+++ lib/templates/mainmenu.dart
@@ -9,34 +9,119 @@ class MainMenu extends StatelessWidget {
           DrawerHeader(
             child: Text(
               '\nINICIO/HOME',
-              style: TextStyle(fontFamily: 'warcraft', fontSize: 40, color: Colors.black,shadows: [
-                                Shadow(
-                                  color: Colors.cyan[300],
-                                  offset: Offset(2, 2),
-                                ),
-                                Shadow(
-                                  color: Colors.cyan[300],
-                                  offset: Offset(-2, 2),
-                                ),
-                                Shadow(
-                                  color: Colors.cyan[300],
-                                  offset: Offset(2, -2),
-                                ),
-                                Shadow(
-                                  color: Colors.cyan[300],
-                                  offset: Offset(-2, -2),
-                                )
-                              ],),
+              style: TextStyle(
+                fontFamily: 'warcraft',
+                fontSize: 40,
+                color: Colors.black,
+                shadows: [
+                  Shadow(
+                    color: Colors.cyan[300],
+                    offset: Offset(2, 2),
+                  ),
+                  Shadow(
+                    color: Colors.cyan[300],
+                    offset: Offset(-2, 2),
+                  ),
+                  Shadow(
+                    color: Colors.cyan[300],
+                    offset: Offset(2, -2),
+                  ),
+                  Shadow(
+                    color: Colors.cyan[300],
+                    offset: Offset(-2, -2),
+                  )
+                ],
+              ),
               textAlign: TextAlign.center,
             ),
-            
             decoration: BoxDecoration(
               color: Color(0xff171a1b),
-              image: DecorationImage(image: AssetImage('assets/images/mainmenu/mainicon.png'),
+              image: DecorationImage(
+                image: AssetImage('assets/images/mainmenu/mainicon.png'),
                 fit: BoxFit.fitWidth,
               ),
             ),
           ),
+          new Container(
+            color: Color(0xff171a1b),
+            child: Column(
+              children: [
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selhome.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selclassic.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/seltbc.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selwotlk.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selcata.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selmop.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selwod.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/sellegion.png'),
+                  ),
+                ),
+                new Text(''),
+                new FlatButton(
+                  onPressed: (){},
+                  child: Image(
+                    image: AssetImage(
+                        'assets/images/mainmenu/selectores/selbfa.png'),
+                  ),
+                ),
+                new Text(''),
+              ],
+            ),
+          )
         ],
       ),
     );
